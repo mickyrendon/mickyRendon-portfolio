06ac3ce4512bb3413b86ae6dd4aef24598aa5dcc
micky rendon <elderendon@hotmail.es>
2022-04-18 00:45:13 -0300

    adding buttons on lable proyects and add animate cdn

--- js/gallery.js
+++ js/gallery.js
@@ -9,9 +9,11 @@ function click(e){
         e.parentElement.classList.remove('active');
         e.classList.remove('shadow');
         e.classList.remove('column');
+        e.firstElementChild.classList.remove('icon-active');
     })
     e.target.parentElement.classList.add('active');
     e.target.classList.add('shadow-active');
     e.target.classList.add('column');
     e.target.classList.add('active-text');
+    e.target.firstElementChild.classList.add('icon-active');
 }
