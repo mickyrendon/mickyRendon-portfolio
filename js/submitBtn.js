4abb13f8ae87cd8defb17894b35340023b9da367
micky rendon <elderendon@hotmail.es>
2022-04-19 15:35:29 -0300

    form finished

---
+++ js/submitBtn.js
@@ -0,0 +1,4 @@
+const submitBtn = document.querySelector('.submit');
+submitBtn.addEventListener('click', e =>{
+   return alert(`You will be redirected to a 'reCAPTCHA' page to comprobate you're not spam`);
+});
